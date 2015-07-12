ee38b1fe87c919b046de6644e9ab4aadf0f4949f
MoPj <wejmps@gmail.com>
2015-07-12 14:02:20 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'vendor/autoload.php';
+date_default_timezone_set ( 'America/New_York');
+
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// $log = new Logger('name');
+// $log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
+// $log->addWarning('Foo');
+$app = new \Slim\Slim();
+
+$app->get('/', function() use($app) {
+  $app->render('index.html');
+  });
+
+$app->get('/contact', function()  use($app) {
+  $app->render('contact.html');
+});
+
+$app->run();
+
+?>
